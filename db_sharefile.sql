dc756773f5de023129ccc84490e3fda0a9250d8e
heartsmile <heart.smile.79@gmail.com>
2015-01-16 16:09:46 +0700

    phan chia thu muc cho user

--- db_sharefile.sql
+++ db_sharefile.sql
@@ -225,3 +225,57 @@ ADD CONSTRAINT `server_detail_fk` FOREIGN KEY (`server_id`) REFERENCES `server` 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2015 at 04:25 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_sharefile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_state`
+--
+
+CREATE TABLE IF NOT EXISTS `file_state` (
+`file_state_id` int(11) NOT NULL,
+  `state_name` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `file_state`
+--
+
+INSERT INTO `file_state` (`file_state_id`, `state_name`) VALUES
+(2, 'uploaded'),
+(3, 'deleted'),
+(4, 'uploadFail'),
+(5, 'recycled');
+
+INSERT INTO `server` (`server_id`, `IP_server`) VALUES
+(1, '104.155.199.62'),
+(2, '107.167.180.164'),
+(3, '104.155.210.44'),
+(4, '130.211.245.75');
+
+--
+-- Indexes for dumped tables
+--
+
